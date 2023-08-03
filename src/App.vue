93d06f996f394590a34306009fd57e0f48d6aed7
Hamed Faryabi <hamed.faryabi9@gmail.com>
2023-08-03 14:28:35 +0330

    Add vue file for test

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { useHeadlessDatePicker } from ".";
+
+const dp = useHeadlessDatePicker();
+
+console.log(dp.getCurrentMonth());
+</script>
+
+<template>
+  <div>hi</div>
+</template>
+
